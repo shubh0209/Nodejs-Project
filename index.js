15e0673f59d2d349b1fb424582aadd88c425d3d7
vikash1sinha <vikash.sinha100@gmail.com>
2020-02-22 23:41:24 +0530

    editing docker-compose

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
      });
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
 });
